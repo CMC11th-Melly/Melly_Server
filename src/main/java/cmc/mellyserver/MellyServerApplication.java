035e225b515594c06c0f7e7cdbacac151b8399d0
jemin <jemin03120111@gmail.com>
2022-09-06 12:30:37 +0900

    init project

---
+++ src/main/java/cmc/mellyserver/MellyServerApplication.java
@@ -0,0 +1,13 @@
+package cmc.mellyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MellyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MellyServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cmc/mellyserver/MellyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cmc.mellyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MellyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
