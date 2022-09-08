4df087c6d81bae0befd39c9f51c52026e4e07335
jemin <jemin03120111@gmail.com>
2022-09-08 15:11:13 +0900

    test: github actions ci/cd 작동 테스트

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+REPOSITORY=/home/ubuntu/melly
+cd $REPOSITORY
+
+APP_NAME=melly
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'mellyServer-0.0.1-SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+CURRENT_PID=$(pgrep -f $APP_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo  "> 종료할 것 없음"
+else
+  echo "> kill -9 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> $JAR_PATH 배포"
+nohup java -jar -Dspring.config.location=/home/ubuntu/melly/src/main/resources/application.yml $JAR_PATH > /home/ubuntu/nohup.out 2>&1 &
+echo "> finished"
+exit 0
