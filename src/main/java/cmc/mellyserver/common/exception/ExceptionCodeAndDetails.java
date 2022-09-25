f07d466f37c023bae46c7b4a10729a435ba93f1f
jemin <jemin03120111@gmail.com>
2022-09-25 23:40:37 +0900

    feat: 회원 가입시 유저 정보 응답 수정

--- src/main/java/cmc/mellyserver/common/exception/ExceptionCodeAndDetails.java
+++ src/main/java/cmc/mellyserver/common/exception/ExceptionCodeAndDetails.java
@@ -9,23 +9,29 @@ import lombok.Getter;
 public enum ExceptionCodeAndDetails {
 
 
+    // HTTP Status : 400 Bad Request
     NOT_FOUND_ERROR_CODE("0001", "발생한 에러의 에러코드를 찾을 수 없습니다."),
-    NOT_FOUND_API("0002", "해당 경로에 대한 응답 API를 찾을 수 없습니다."),
-    BAD_REQUEST("0003", "잘못된 요청입니다."),
+    BAD_REQUEST("0002", "잘못된 요청입니다."),
 
-    // 인증 관련 예외
+    // HTTP Status : 404 Not Found
+    NOT_FOUND_API("0003", "해당 경로에 대한 응답 API를 찾을 수 없습니다."),
+
+    // HTTP Status : 400 Bad Request
     DUPLICATE_EMAIL("1000","중복되는 이메일입니다."),
-    DUPLICATE_NICKNAME("1002","중복되는 닉네임입니다."),
-    INVALID_EMAIL("1001","이메일이 일치하지 않습니다."),
+    DUPLICATE_NICKNAME("1001","중복되는 닉네임입니다."),
+    INVALID_EMAIL("1002","이메일이 일치하지 않습니다."),
     INVALID_PASSWORD("1003","비밀번호가 일치하지 않습니다."),
     NO_SUCH_USER("1004","해당 id의 유저가 없습니다."),
-    UNAUTHORIZED_USER("1005", "인증되지 않은 유저입니다."),
-    KAKAO_ACCESS("1006","카카오 리소스 서버 접근 중 문제가 발생했습니다."),
-    GOOGLE_ACCESS("1007","구글 리소스 서버 접근 중 문제가 발생했습니다."),
-    NAVER_ACCESS("1008","네이버 리소스 서버 접근 중 문제가 발생했습니다."),
-    APPLE_ACCESS("1009","애플 로그인 중 문제가 발생했습니다."),
-    FORBIDDEN_USER("1010","접근 권한이 없는 유저입니다."),
-    EXPIRED_TOKEN("1011", "JWT 토큰 기한이 만료됐습니다.");
+    KAKAO_ACCESS("1005","카카오 리소스 서버 접근 중 문제가 발생했습니다."),
+    GOOGLE_ACCESS("1006","구글 리소스 서버 접근 중 문제가 발생했습니다."),
+    NAVER_ACCESS("1007","네이버 리소스 서버 접근 중 문제가 발생했습니다."),
+    APPLE_ACCESS("1008","애플 로그인 중 문제가 발생했습니다."),
+    // HTTP Status : 401 Unauthorized
+    UNAUTHORIZED_USER("1009", "인증되지 않은 유저입니다."),
+    EXPIRED_TOKEN("1010", "JWT 토큰 기한이 만료됐습니다."),
+
+    // HTTP Status : 403 Forbidden
+    FORBIDDEN_USER("1011","접근 권한이 없는 유저입니다.");
 
 
     private final String code;
