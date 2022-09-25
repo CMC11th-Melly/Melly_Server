dab6d20274d60f166613ecac90b6c29c74f93a43
jemin <jemin03120111@gmail.com>
2022-09-25 12:42:58 +0900

    feat: 애플 로그인 검증 로직 추가

--- src/main/java/cmc/mellyserver/auth/client/AppleClient.java
+++ src/main/java/cmc/mellyserver/auth/client/AppleClient.java
@@ -1,57 +1,106 @@
 package cmc.mellyserver.auth.client;
 
 
+import cmc.mellyserver.auth.client.dto.ApplePublicKeyResponse;
+import cmc.mellyserver.auth.exception.TokenValidFailedException;
 import cmc.mellyserver.auth.presentation.dto.Provider;
+import cmc.mellyserver.common.exception.ExceptionCodeAndDetails;
+import cmc.mellyserver.common.exception.GlobalBadRequestException;
+import cmc.mellyserver.common.exception.GlobalServerException;
 import cmc.mellyserver.user.domain.User;
-
-import com.nimbusds.jose.shaded.json.JSONObject;
-import com.nimbusds.jwt.JWTClaimsSet;
-import com.nimbusds.jwt.SignedJWT;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-
-
+import reactor.core.publisher.Mono;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.RSAPublicKeySpec;
+import java.util.Base64;
 import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
 public class AppleClient implements Client{
 
+    private final WebClient webClient;
 
     @Override
     public User getUserData(String accessToken) {
 
-        JSONObject jsonObject = decodeFromIdToken(accessToken);
-        System.out.println("최종 응답값 : " + jsonObject.toJSONString());
-        String sub = (String)jsonObject.get("sub");
-        String email = (String)jsonObject.get("email");
-
-        return User.builder()
-                .userId(sub)
-                .email(email)
-                .provider(Provider.APPLE).build();
-
-    }
-    public JSONObject decodeFromIdToken(String id_token) {
+        // response를 가져오는 과정
+        ApplePublicKeyResponse response = webClient.get()
+                .uri("https://appleid.apple.com/auth/keys")
+                .retrieve()
+                .onStatus(HttpStatus::is4xxClientError, clientResponse -> Mono.error(new GlobalBadRequestException(ExceptionCodeAndDetails.KAKAO_ACCESS)))
+                .onStatus(HttpStatus::is5xxServerError, clientResponse -> Mono.error(new TokenValidFailedException("Initail Server error")))
+                .bodyToMono(ApplePublicKeyResponse.class)
+                .block();
 
         try {
-            SignedJWT signedJWT = SignedJWT.parse(id_token);
-            JWTClaimsSet getPayload = signedJWT.getJWTClaimsSet();
-            Map<String, Object> stringObjectMap = getPayload.toJSONObject();
-            for (String s : stringObjectMap.keySet()) {
-                System.out.println("무슨 값이 들어있지? " + stringObjectMap.get(s));
-            }
-            JSONObject payload = new JSONObject(stringObjectMap);
+            // jwt header 값 가져오고
+            String headerOfIdentityToken = accessToken.substring(0, accessToken.indexOf("."));
 
-            if (payload != null) {
-                return payload;
-            }
-        } catch (Exception e) {
+            Map<String, String> header = new ObjectMapper().readValue(new String(Base64.getDecoder().decode(headerOfIdentityToken), "UTF-8"), Map.class);
+
+            // 지금 받아온 공개키 중에서 내가 가져온 id_token의 kid와 alg과 같은게 있다면 그 키를 사용
+            ApplePublicKeyResponse.Key key = response.getMatchedKeyBy(header.get("kid"), header.get("alg"))
+                    .orElseThrow(() -> new GlobalServerException());
+
+            byte[] nBytes = Base64.getUrlDecoder().decode(key.getN());
+            byte[] eBytes = Base64.getUrlDecoder().decode(key.getE());
+
+            BigInteger n = new BigInteger(1, nBytes);
+            BigInteger e = new BigInteger(1, eBytes);
+
+            RSAPublicKeySpec publicKeySpec = new RSAPublicKeySpec(n, e);
+            KeyFactory keyFactory = KeyFactory.getInstance(key.getKty());
+            PublicKey publicKey = keyFactory.generatePublic(publicKeySpec);
+
+            Claims body = Jwts.parser().setSigningKey(publicKey).parseClaimsJws(accessToken).getBody();
+
+            System.out.println(body);
+            String sub = body.getSubject();
+            String email = (String)body.get("email");
+
+            return User.builder()
+                        .userId(sub)
+                         .email(email)
+                        .provider(Provider.APPLE).build();
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace(); }
+        catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        } catch (MalformedJwtException e) {
             e.printStackTrace();
         }
-
+        catch (ExpiredJwtException e) {
+            e.printStackTrace();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("apple 로그인 유저 없음!");
         return null;
+
     }
 
 }
---
+++ src/main/java/cmc/mellyserver/auth/client/dto/ApplePublicKeyResponse.java
@@ -0,0 +1,30 @@
+package cmc.mellyserver.auth.client.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Optional;
+
+@Data
+public class ApplePublicKeyResponse {
+    private List<Key> keys;
+
+    @Getter
+    @Setter
+    public static class Key {
+        private String kty;
+        private String kid;
+        private String use;
+        private String alg;
+        private String n;
+        private String e;
+    }
+
+    public Optional<Key> getMatchedKeyBy(String kid, String alg) {
+        return this.keys.stream()
+                .filter(key -> key.getKid().equals(kid) && key.getAlg().equals(alg))
+                .findFirst();
+    }
+}
