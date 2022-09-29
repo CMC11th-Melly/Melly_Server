b35abd5d9cf874207b2cdb16ab2d90b700b04b6d
jemin <jemin03120111@gmail.com>
2022-09-29 16:14:26 +0900

    test

--- src/main/java/cmc/mellyserver/group/application/GroupService.java
+++ src/main/java/cmc/mellyserver/group/application/GroupService.java
@@ -1,5 +1,6 @@
 package cmc.mellyserver.group.application;
 
+import cmc.mellyserver.group.domain.GroupRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,5 +8,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class GroupService {
 
+    private final GroupRepository groupRepository;
 
+    // group 만들때 더해야 하는 것
+    // 일단 그룹 생성자와 그룹 멤버가 빠지면 안되지
+    public void saveGroup()
+    {g
+
+    }
 }
