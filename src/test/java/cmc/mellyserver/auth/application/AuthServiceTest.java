5374154e9389301d47a50ea177dbba70ef8c4178
jemin <jemin03120111@gmail.com>
2022-10-02 01:55:31 +0900

    test: beanstalk 환경 배포

--- src/test/java/cmc/mellyserver/auth/application/AuthServiceTest.java
+++ src/test/java/cmc/mellyserver/auth/application/AuthServiceTest.java
@@ -1,46 +1,78 @@
 package cmc.mellyserver.auth.application;
 
 import cmc.mellyserver.auth.token.JwtTokenProvider;
+import cmc.mellyserver.common.exception.GlobalBadRequestException;
+import cmc.mellyserver.common.util.aws.AWSS3UploadService;
 import cmc.mellyserver.user.domain.User;
 import cmc.mellyserver.user.domain.UserRepository;
-import cmc.mellyserver.user.domain.enums.RoleType;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
-@Import(JwtTokenProvider.class)
 class AuthServiceTest {
 
-//    @InjectMocks
-//    private AuthService authService;
-//
-//    @Mock
-//    private UserRepository userRepository;
-//
-//
-//    @Test
-//    @DisplayName("일반 로그인 테스트")
-//    void normal_login()
-//    {
-//        BDDMockito.given(userRepository.findUserByEmail(any()))
-//                .willReturn(Optional.of(User.builder().uid("cmc11th").email("melly@gmail.com").roleType(RoleType.USER).password("asdfasdf").build()));
-//
-//        System.out.println("hello");
-//        authService.login("melly@gmail.com","asdfasdf");
-//    }
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+    @Mock
+    private JwtTokenProvider jwtTokenProvider;
+    @Mock
+    private RedisTemplate redisTemplate;
+    @Mock
+    private AWSS3UploadService uploadService;
+
+    @InjectMocks
+    private AuthService authService;
+
+
+    @Test
+    @DisplayName("이메일이나 비밀번호가 다르면 예외가 발생한다.")
+    void checkLogin()
+    {
+        given(userRepository.findUserByEmail(any())).willReturn(Optional.empty());
+
+        Assertions.assertThatThrownBy(()->{
+            authService.login("melly@gmail.com","asdfasdf");
+        }).isInstanceOf(GlobalBadRequestException.class);
+    }
+
+    @Test
+    @DisplayName("회원가입 시에 닉네임 중복이 있는지 체크한다.")
+    void checkDuplicatedNickname()
+    {
+        given(userRepository.findUserByNickname(any())).willReturn(Optional.of(User.builder().build()));
+
+        Assertions.assertThatThrownBy(()->{
+            authService.checkNicknameDuplicate("nickname");
+        }).isInstanceOf(GlobalBadRequestException.class);
+    }
+
+    @Test
+    @DisplayName("회원가입 시에 이메일 중복이 있는지 체크한다.")
+    void checkDuplicatedEmail()
+    {
+        given(userRepository.findUserByEmail(any())).willReturn(Optional.of(User.builder().build()));
+
+        Assertions.assertThatThrownBy(()->{
+            authService.checkDuplicatedEmail("email");
+        }).isInstanceOf(GlobalBadRequestException.class);
+    }
 
 
 }
