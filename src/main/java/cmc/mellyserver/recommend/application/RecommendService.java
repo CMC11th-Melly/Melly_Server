fde7275d7b48d8abe02065376048c18ace032319
jemin <jemin03120111@gmail.com>
2022-10-02 21:41:38 +0900

    feat: 핫한 장소 테스트 데이터 추가

--- src/main/java/cmc/mellyserver/recommend/application/RecommendService.java
+++ src/main/java/cmc/mellyserver/recommend/application/RecommendService.java
@@ -1,10 +1,12 @@
 package cmc.mellyserver.recommend.application;
 
+import cmc.mellyserver.group.domain.GroupRepository;
 import cmc.mellyserver.group.domain.enums.GroupType;
 import cmc.mellyserver.place.domain.Place;
 import cmc.mellyserver.place.domain.PlaceQueryRepository;
 import cmc.mellyserver.place.domain.PlaceRepository;
 import cmc.mellyserver.recommend.application.dto.RecommendResponseDto;
+import cmc.mellyserver.trend.application.dto.TrendResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,19 +20,22 @@ import java.util.stream.Collectors;
 public class RecommendService {
 
     private final PlaceRepository placeRepository;
+    private final GroupRepository groupRepository;
     private final PlaceQueryRepository placeQueryRepository;
     public List<RecommendResponseDto> getRecommend()
     {
         List<Place> recommendPlace = placeQueryRepository.getRecommendPlace(List.of(1L, 2L, 3L));
         return recommendPlace.stream().map
-                (r -> new RecommendResponseDto(r.getId(),r.getPlaceImage(),
+                (t -> new RecommendResponseDto(t.getId(),t.getPlaceImage(),
                         GroupType.FRIEND,
                         false,
-                        r.getName(),
-                        r.getMemories().get(0).getId(),
-                        r.getMemories().get(0).getMemoryImages().get(0).getImagePath(),
-                        r.getMemories().get(0).getTitle(),
-                        r.getMemories().get(0).getContent()))
+                        t.getName(),
+                        t.getMemories().get(0).getId(),
+                        t.getMemories().get(0).getMemoryImages().stream().map(tm -> tm.getImagePath()).collect(Collectors.toList()),
+                        t.getMemories().get(0).getTitle(),
+                        t.getMemories().get(0).getContent(),
+                        groupRepository.findById(t.getMemories().get(0).getGroupInfo().getGroupId()).get().getGroupName(),
+                        t.getMemories().get(0).getStars()))
                 .collect(Collectors.toList());
     }
 }
--- src/main/java/cmc/mellyserver/recommend/application/dto/RecommendResponseDto.java
+++ src/main/java/cmc/mellyserver/recommend/application/dto/RecommendResponseDto.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -12,10 +13,10 @@ public class RecommendResponseDto implements Serializable {
     private PlaceInfo placeInfo;
     private MemoryInfo memoryInfo;
 
-    public RecommendResponseDto(Long placeId,String placeImage, GroupType placeCategory,Boolean isScraped,String placeName,Long memoryId,String memoryImage, String title, String content)
+    public RecommendResponseDto(Long placeId, String placeImage, GroupType placeCategory, Boolean isScraped, String placeName, Long memoryId, List<String> memoryImages, String title, String content, String groupName, Integer stars)
     {
         this.placeInfo = new PlaceInfo(placeId,placeImage,placeCategory,isScraped,placeName);
-        this.memoryInfo = new MemoryInfo(memoryId,memoryImage, title,content);
+        this.memoryInfo = new MemoryInfo(memoryId,memoryImages, title,content,groupName,stars,List.of("최고에요","재밌어요"));
     }
 
     @Data
@@ -39,16 +40,22 @@ public class RecommendResponseDto implements Serializable {
     @Data
     static class MemoryInfo implements Serializable{
         private Long memoryId;
-        private String memoryImage;
+        private List<String> memoryImages;
         private String title;
         private String content;
+        private String groupName;
+        private Integer stars;
+        private List<String> keywords;
 
-        public MemoryInfo(Long memoryId,String memoryImage, String title, String content)
+        public MemoryInfo(Long memoryId,List<String> memoryImages, String title, String content,String groupName,Integer stars, List<String> keywords)
         {
             this.memoryId = memoryId;
-            this.memoryImage = memoryImage;
+            this.memoryImages = memoryImages;
             this.title = title;
             this.content = content;
+            this.groupName = groupName;
+            this.stars =stars;
+            this.keywords = keywords;
         }
     }
 
