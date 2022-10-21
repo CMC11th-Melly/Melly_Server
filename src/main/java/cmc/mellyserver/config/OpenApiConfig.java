6f1664d077183f1dfc335b0daf9989e03166232e
jemin <jemin03120111@gmail.com>
2022-10-21 11:50:48 +0900

    feat: swagger 문서화 수정

--- src/main/java/cmc/mellyserver/config/OpenApiConfig.java
+++ src/main/java/cmc/mellyserver/config/OpenApiConfig.java
@@ -6,11 +6,13 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
+import java.util.List;
 
 
 @Configuration
@@ -31,11 +33,13 @@ public class OpenApiConfig {
 
         SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
 
-
+        Server server1 = new Server();
+        server1.setUrl("https://api.melly.kr");
         return new OpenAPI()
                 .components(new Components())
                 .components(new Components().addSecuritySchemes("bearerAuth",securityScheme))
                 .security(Arrays.asList(securityRequirement))
+                .servers(List.of(server1))
                 .info(info);
 
 
