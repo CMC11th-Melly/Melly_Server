e7772af81be2b5d7fbbb383ae0f6f5c7ec79409e
jemin <jemin03120111@gmail.com>
2022-11-09 02:27:12 +0900

    refactor: 추천/핫한 장소 조회 로직에 장소 스크랩 여부 체크 로직 추가

--- src/main/java/cmc/mellyserver/trend/application/TrendService.java
+++ src/main/java/cmc/mellyserver/trend/application/TrendService.java
@@ -7,6 +7,7 @@ import cmc.mellyserver.place.domain.Place;
 import cmc.mellyserver.place.domain.PlaceQueryRepository;
 import cmc.mellyserver.trend.application.dto.TrendResponseDto;
 import cmc.mellyserver.trend.infrastructure.TrendAnalyzer;
+import cmc.mellyserver.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,18 +26,24 @@ public class TrendService {
     private final AuthenticatedUserChecker authenticatedUserChecker;
 
     public List<TrendResponseDto> getTrend(String uid) {
+           User user = authenticatedUserChecker.checkAuthenticatedUserExist(uid);
            List<Place> trendingPlace = placeQueryRepository.getTrendingPlace(List.of(1L, 2L, 3L));
-//        User user = authenticatedUserChecker.checkAuthenticatedUserExist(uid);
+
 //        return trendAnalyzer.findKeywordSortedByRank(user);
         return trendingPlace.stream().map
                 (t -> new TrendResponseDto(t.getId(),t.getPlaceImage(),
                         t.getPlaceCategory(),
                         GroupType.FRIEND,
-                        t.getIsScraped(),
+                        checkIsScraped(user,t),
                         t.getPlaceName(),t.getMemories(),
                         groupRepository.findById(t.getMemories().get(0).getGroupInfo().getGroupId()).get().getGroupName()
                 ))
                 .collect(Collectors.toList());
+    }
+
+    private boolean checkIsScraped(User user, Place place)
+    {
+        return user.getPlaceScraps().stream().anyMatch(s -> s.getPlace().getId().equals(place.getId()));
     }
 
 }
