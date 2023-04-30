993142c43c8048c992ee6aeb7de7a1d3856e7e2a
jemin <jemin03120111@gmail.com>
2023-05-01 02:50:56 +0900

    memory 반환 DTO 형식 통일

--- src/main/java/cmc/mellyserver/memory/presentation/dto/wrapper/GetMyGroupMemoryForPlaceResponseWrapper.java
+++ src/main/java/cmc/mellyserver/memory/presentation/dto/wrapper/GetMyGroupMemoryForPlaceResponseWrapper.java
@@ -1,6 +1,7 @@
 package cmc.mellyserver.memory.presentation.dto.wrapper;
 
 import cmc.mellyserver.memory.application.dto.response.MemoryForGroupResponse;
+import cmc.mellyserver.memory.domain.dto.MemoryResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.domain.Slice;
@@ -9,5 +10,5 @@ import org.springframework.data.domain.Slice;
 @AllArgsConstructor
 public class GetMyGroupMemoryForPlaceResponseWrapper {
     private Long memoryCount;
-    private Slice<MemoryForGroupResponse> memoryList;
+    private Slice<MemoryResponseDto> memoryList;
 }
