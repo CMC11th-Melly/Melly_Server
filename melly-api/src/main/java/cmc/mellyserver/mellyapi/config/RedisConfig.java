e57be5ae8dc99c34ca3bb57f41453c8cb972237d
jemin <jemin03120111@gmail.com>
2023-06-14 00:42:23 +0900

    test: testContainers 설치

--- melly-api/src/main/java/cmc/mellyserver/mellyapi/config/RedisConfig.java
+++ melly-api/src/main/java/cmc/mellyserver/mellyapi/config/RedisConfig.java
@@ -3,6 +3,7 @@ package cmc.mellyserver.mellyapi.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -11,6 +12,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 
 @Configuration
+@Profile("local")
 @EnableRedisRepositories
 public class RedisConfig {
 
