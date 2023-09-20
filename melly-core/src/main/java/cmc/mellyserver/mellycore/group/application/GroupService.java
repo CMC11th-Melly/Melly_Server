f1ecf434a07e2db31de5ddd4cc41a769b081fa71
jemin <jemin03120111@gmail.com>
2023-09-20 23:42:25 +0900

    refactor: 간격 수정

--- melly-core/src/main/java/cmc/mellyserver/mellycore/group/application/GroupService.java
+++ melly-core/src/main/java/cmc/mellyserver/mellycore/group/application/GroupService.java
@@ -20,11 +20,10 @@ import cmc.mellyserver.mellycore.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
-import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -35,6 +34,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class GroupService {
 
+    private static final int GROUP_MEMBER_MAX_COUNT = 9;
+
     private final GroupRepository groupRepository;
 
     private final GroupAndUserRepository groupAndUserRepository;
@@ -44,9 +45,6 @@ public class GroupService {
     private final UserGroupQueryRepository userGroupQueryRepository;
 
 
-    /*
-    캐시 적용 여부 : 가능
-     */
     @Cacheable(value = "group:group-id", key = "#groupId")
     @Transactional(readOnly = true)
     public GroupDetailResponseDto getGroupDetail(final Long groupId) {
@@ -57,10 +55,6 @@ public class GroupService {
     }
 
 
-    /*
-    캐시 적용 여부 : 불가능
-    이유 : 정확한 데이터를 반영해야함으로 직접 DB에서 조회해야 한다.
-     */
     @Transactional(readOnly = true)
     public GroupListLoginUserParticipatedResponse findGroupListLoginUserParticiated(final Long userId, final Long groupId, final Pageable pageable) {
 
@@ -81,35 +75,31 @@ public class GroupService {
         return savedGroup.getId();
     }
 
-
-    @Retryable(maxAttempts = 3, include = OptimisticLockingFailureException.class)
-    @CacheEvict(value = "group:group-id", key = "#groupId")
-    @DistributedLock(key = "#groupId")
+    // TODO : 모든 어노테이션이 AOP를 기반으로 동작한다. 각각의 AOP 동작 순서 파악하기
+    @CachePut(value = "group:group-id", key = "#groupId")
+    @DistributedLock(key = "#groupId", waitTime = 10L, leaseTime = 2L)
     @OptimisticLock
     @Transactional
     public void participateToGroup(final Long userId, final Long groupId) {
 
         User user = userRepository.getById(userId);
         UserGroup userGroup = groupRepository.getById(groupId);
-        Integer particiatedUserCount = groupAndUserRepository.countUserParticipatedInGroup(groupId);
 
-        if (particiatedUserCount > 9) {
-            throw new BusinessException(ErrorCode.PARTICIPATE_GROUP_NOT_POSSIBLE);
-        }
-
+        validateNewUserParticipateEnable(groupId);
         checkUserAlreadyParticipatedInGroup(user, userGroup);
+
         groupAndUserRepository.save(GroupAndUser.of(user, userGroup));
     }
 
-
+    @CachePut(value = "group:group-id", key = "#updateGroupRequestDto.groupId")
     @OptimisticLock
-    @CacheEvict(value = "group:group-id", key = "#updateGroupRequestDto.groupId")
     @Transactional
     public void updateGroup(final UpdateGroupRequestDto updateGroupRequestDto) {
 
         UserGroup userGroup = groupRepository.getById(updateGroupRequestDto.getGroupId());
         userGroup.update(updateGroupRequestDto.getGroupName(), updateGroupRequestDto.getGroupType(), updateGroupRequestDto.getGroupIcon());
     }
+
 
     @CacheEvict(value = "group:group-id", key = "#groupId")
     @Transactional
@@ -119,14 +109,12 @@ public class GroupService {
         userGroup.remove();
     }
 
+
     @CacheEvict(value = "group:group-id", key = "#groupId")
     @Transactional
     public void exitGroup(final Long userId, final Long groupId) {
 
-        Integer particiatedUserCount = groupAndUserRepository.countUserParticipatedInGroup(groupId);
-
-        log.info("count : {}", particiatedUserCount);
-
+        int particiatedUserCount = groupAndUserRepository.countUserParticipatedInGroup(groupId);
 
         if (particiatedUserCount < 2) {
             removeGroup(userId, groupId);
@@ -135,9 +123,19 @@ public class GroupService {
         groupAndUserRepository.deleteGroupAndUserByUserIdAndGroupId(userId, groupId);
     }
 
+
     private void checkUserAlreadyParticipatedInGroup(User user, UserGroup userGroup) {
         if (groupAndUserRepository.findGroupAndUserByUserAndGroup(user, userGroup).isPresent()) {
             throw new BusinessException(ErrorCode.DUPLICATED_GROUP);
+        }
+    }
+
+
+    private void validateNewUserParticipateEnable(Long groupId) {
+        int particiatedUserCount = groupAndUserRepository.countUserParticipatedInGroup(groupId);
+
+        if (particiatedUserCount > GROUP_MEMBER_MAX_COUNT) {
+            throw new BusinessException(ErrorCode.PARTICIPATE_GROUP_NOT_POSSIBLE);
         }
     }
 }
