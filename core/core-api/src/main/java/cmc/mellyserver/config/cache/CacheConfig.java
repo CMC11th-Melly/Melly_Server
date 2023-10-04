af68e3cb55eee1c9793a63bbb8cef6620d078968
jemin <jemin03120111@gmail.com>
2023-10-05 02:32:40 +0900

    refactor: CacheConfig내 key Serializer 삭제
    key serializer를 StringRedisSerializer로 설정 시 저장이 안되는 문제가 발생했습니다.

--- core/core-api/src/main/java/cmc/mellyserver/config/cache/CacheConfig.java
+++ core/core-api/src/main/java/cmc/mellyserver/config/cache/CacheConfig.java
@@ -13,7 +13,6 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
 
@@ -63,7 +62,6 @@ public class CacheConfig {
         RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofSeconds(60))
                 .disableCachingNullValues()
-                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper())));
 
         return RedisCacheManager.builder(redisConnectionFactory)
