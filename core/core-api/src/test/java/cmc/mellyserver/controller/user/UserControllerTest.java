b98ec71895976772a57c1b9ad7cad80342e5483a
jemin <jemin03120111@gmail.com>
2023-10-27 15:00:50 +0900

    refactor: sonarCloud 적용 테스트 (#119)

--- core/core-api/src/test/java/cmc/mellyserver/controller/user/UserControllerTest.java
+++ core/core-api/src/test/java/cmc/mellyserver/controller/user/UserControllerTest.java
@@ -30,24 +30,23 @@ public class UserControllerTest extends ControllerTest {
 
 		// given
 		given(userProfileService.getProfile(anyLong()))
-				.willReturn(ProfileResponseDto.of(1L, 모카().getNickname(), 모카().getEmail(), 모카().getProfileImage()));
+			.willReturn(ProfileResponseDto.of(1L, 모카().getNickname(), 모카().getEmail(), 모카().getProfileImage()));
 		given(userProfileService.checkImageStorageVolume(anyLong())).willReturn(1);
 
 		// when & then
 		mockMvc
-				.perform(get("/api/users/my-profile").accept(MediaType.APPLICATION_JSON)
-						.contentType(MediaType.APPLICATION_JSON))
-				.andDo(print())
-				.andDo(document("user/profile", preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()),
-						responseFields(fieldWithPath("code").type(JsonFieldType.NUMBER).description("HTTP 상태 코드"),
-								fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메세지"),
-								fieldWithPath("data").type(JsonFieldType.OBJECT).description("응답 데이터"),
-								fieldWithPath("data.userId").type(JsonFieldType.NUMBER).description("유저 ID"),
-								fieldWithPath("data.nickname").type(JsonFieldType.STRING).description("닉네임"),
-								fieldWithPath("data.profileImage").type(JsonFieldType.STRING)
-										.description("프로필 이미지 URL"),
-								fieldWithPath("data.imageVolume").type(JsonFieldType.NUMBER)
-										.description("유저 이미지 총 용량"))))
-				.andExpect(status().isOk());
-}
+			.perform(get("/api/users/my-profile").accept(MediaType.APPLICATION_JSON)
+				.contentType(MediaType.APPLICATION_JSON))
+			.andDo(print())
+			.andDo(document("user/profile", preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()),
+					responseFields(fieldWithPath("code").type(JsonFieldType.NUMBER).description("HTTP 상태 코드"),
+							fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메세지"),
+							fieldWithPath("data").type(JsonFieldType.OBJECT).description("응답 데이터"),
+							fieldWithPath("data.userId").type(JsonFieldType.NUMBER).description("유저 ID"),
+							fieldWithPath("data.nickname").type(JsonFieldType.STRING).description("닉네임"),
+							fieldWithPath("data.profileImage").type(JsonFieldType.STRING).description("프로필 이미지 URL"),
+							fieldWithPath("data.imageVolume").type(JsonFieldType.NUMBER).description("유저 이미지 총 용량"))))
+			.andExpect(status().isOk());
 	}
+
+}
