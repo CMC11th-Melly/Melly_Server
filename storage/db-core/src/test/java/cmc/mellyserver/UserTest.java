07ad08ec2daab674de811cf6ef9feeb806da0f03
jemin <jemin03120111@gmail.com>
2023-10-27 18:43:49 +0900

    style: UserTest formatting 적용

--- storage/db-core/src/test/java/cmc/mellyserver/UserTest.java
+++ storage/db-core/src/test/java/cmc/mellyserver/UserTest.java
@@ -7,17 +7,18 @@ import org.junit.jupiter.api.DisplayName;
 
 public class UserTest {
 
-    @DisplayName("유저의 프로필 이미지를 수정한다.")
-    @Test
-    public void 유저의_프로필_이미지를_변경한다() {
+	@DisplayName("유저의 프로필 이미지를 수정한다.")
+	@Test
+	public void 유저의_프로필_이미지를_변경한다() {
 
-        // given
-        User user = User.builder().build();
+		// given
+		User user = User.builder().build();
 
-        // when
-        user.changeProfileImage("updated_image.png");
+		// when
+		user.changeProfileImage("updated_image.png");
 
-        // then
-        Assertions.assertThat(user.getProfileImage()).isEqualTo("updated_image.png");
-    }
+		// then
+		Assertions.assertThat(user.getProfileImage()).isEqualTo("updated_image.png");
+	}
+
 }
