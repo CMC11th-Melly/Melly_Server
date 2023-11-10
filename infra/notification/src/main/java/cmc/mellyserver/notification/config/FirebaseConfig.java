ca2557377166829f796a2dd9e7fb27f1094e2d61
jemin <jemin03120111@gmail.com>
2023-11-11 01:08:42 +0900

    test: jacoco aggregation 설정

--- infra/notification/src/main/java/cmc/mellyserver/notification/config/FirebaseConfig.java
+++ infra/notification/src/main/java/cmc/mellyserver/notification/config/FirebaseConfig.java
@@ -1,30 +1,41 @@
 package cmc.mellyserver.notification.config;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 import java.io.FileInputStream;
 import java.io.IOException;
+
 import javax.annotation.PostConstruct;
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Configuration
-@Profile({ "local" })
 public class FirebaseConfig {
 
 	@PostConstruct
 	public void initializeFCM() throws IOException {
 
-		Resource resource = new ClassPathResource("firebase-key.json");
-		FileInputStream fis = new FileInputStream(resource.getFile());
+		try {
+			Resource resource = new ClassPathResource("firebase-key.json");
+			FileInputStream fis = new FileInputStream(resource.getFile());
 
-		FirebaseOptions options = new FirebaseOptions.Builder().setCredentials(GoogleCredentials.fromStream(fis))
-			.build();
+			FirebaseOptions options = new FirebaseOptions.Builder().setCredentials(GoogleCredentials.fromStream(fis))
+				.build();
 
-		FirebaseApp.initializeApp(options);
+			if (FirebaseApp.getApps().isEmpty()) {
+				FirebaseApp.initializeApp(options);
+			}
+		} catch (IOException e) {
+			log.error("Failed initializing FilebaseApp: {}", e.getMessage());
+		}
+
 	}
 
 }
