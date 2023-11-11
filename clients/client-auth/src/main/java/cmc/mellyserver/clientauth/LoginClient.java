90bc1ab56e40b4c1e23c102b82f4b5636e94329c
jemin <jemin03120111@gmail.com>
2023-11-11 16:00:05 +0900

    docs: client-auth 모듈 주석 추가

--- clients/client-auth/src/main/java/cmc/mellyserver/clientauth/LoginClient.java
+++ clients/client-auth/src/main/java/cmc/mellyserver/clientauth/LoginClient.java
@@ -2,6 +2,13 @@ package cmc.mellyserver.clientauth;
 
 import cmc.mellyserver.clientauth.api.LoginClientResult;
 
+/*
+현재 Melly에서는 카카오, 네이버, 구글, 애플 OAuth를 사용하고 있습니다. 차후 OAuth는 얼마든지 추가될 수 있기에 확장성을 고려한 설계가 필요합니다.
+따라서 LoginClient 인터페이스를 만들어서 구체 클라이언트들이 구현하도록 만들었습니다.
+OAuth 리소스 서버로부터 데이터를 받아와야 하는 쪽에서는 누구에게 요청하는지 관심을 가지지 않아도 됩니다.
+- support : 해당 클라이언트가 어떤 Provider인지 판별합니다
+- getUserData : 실제 유저 데이터를 받아옵니다
+ */
 public interface LoginClient {
 
 	boolean supports(String provider);
--- clients/client-auth/src/main/java/cmc/mellyserver/clientauth/api/NaverLoginApi.java
+++ clients/client-auth/src/main/java/cmc/mellyserver/clientauth/api/NaverLoginApi.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RequestHeader;
 
 import cmc.mellyserver.clientauth.model.NaverResource;
 
+/*
+통신 클라이언트를 호출하는 작업은 OpenFeign 인터페이스가 아닌 NaverClient라는 객체를 통해 수행합니다.
+OpenFeign 인터페이스에 대한 세부 구현은 package default 접근 지시자를 통해 예상치 못한 곳에서의 사용을 차단했습니다.
+ */
 @FeignClient(name = "naverLogin", url = "${client.oauth.uri.naver}")
 interface NaverLoginApi {
 
