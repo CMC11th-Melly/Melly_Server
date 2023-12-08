0866fa3d9783815fd89f89e601f9b1ff9a7bb5fd
jemin <jemin03120111@gmail.com>
2023-12-09 01:17:03 +0900

    cicd: Dockerfile 경로 수정 테스트

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,14 +3,11 @@
 # 작업 디렉토리를 /home/ec2-user/app으로 변경
 cd /home/ec2-user/app
 
-# 환경변수 DOCKER_APP_NAME을 spring으로 설정
 DOCKER_APP_NAME=melly
 
-# 실행중인 blue가 있는지 확인
-# 프로젝트의 실행 중인 컨테이너를 확인하고, 해당 컨테이너가 실행 중인지 여부를 EXIST_BLUE 변수에 저장
-EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml ps | grep Up)
+EXIST_SERVICE=$(sudo docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml ps | grep Up)
 
-if [ -z "$EXIST_BLUE" ]; then
+if [ -z "$EXIST_SERVICE" ]; then
 	sudo docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml up -d --build
 else
   sudo docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml down
