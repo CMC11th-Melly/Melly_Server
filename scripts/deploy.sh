42b0bba2a10616a7c0053b7e682b95f47d800e68
jemin <jemin03120111@gmail.com>
2023-12-09 00:33:49 +0900

    cicd: 배포 관련 파일 정리

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,15 +8,13 @@ DOCKER_APP_NAME=spring
 
 # 실행중인 blue가 있는지 확인
 # 프로젝트의 실행 중인 컨테이너를 확인하고, 해당 컨테이너가 실행 중인지 여부를 EXIST_BLUE 변수에 저장
-EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml ps | grep Up)
 
-# green이 실행중이면 blue up
-# EXIST_BLUE 변수가 비어있는지 확인
 if [ -z "$EXIST_BLUE" ]; then
-	sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
+	sudo docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml up -d --build
 else
-  sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+  sudo docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml down
   sudo docker image prune -af
-  sleep 20
-  sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
+  sleep 10
+  sudo docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml up -d --build
 fi
