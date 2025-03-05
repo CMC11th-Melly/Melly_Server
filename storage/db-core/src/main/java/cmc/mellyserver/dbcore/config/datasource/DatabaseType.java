0f97b5d31884baa978455370dc3f2498238546c2
Jemin <jemin9812@gmail.com>
2025-03-05 22:59:13 +0900

    fix: remove unnecessary directory

--- storage/db-core/src/main/java/cmc/mellyserver/dbcore/config/datasource/DatabaseType.java
+++
@@ -1,9 +0,0 @@
-package cmc.mellyserver.dbcore.config.datasource;
-
-public abstract class DatabaseType {
-
-    public static final String SOURCE = "source";
-
-    public static final String REPLICA = "replica";
-
-}
